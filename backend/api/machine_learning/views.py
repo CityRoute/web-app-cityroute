29147be516d7cf4f9700b1d2d5841555e1070b49
Brian Manning <manningbrian98@gmail.com>
2021-08-04 18:04:16 +0100

    finish getallstops function which returns list of stops

--- backend/api/machine_learning/views.py
+++ backend/api/machine_learning/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from backend.api.models import Route, Stop, RouteStop
+from backend.api.serializer import StopSerializer
 
 @api_view(['GET'])
 def StopToStopModelView(request):
@@ -13,18 +14,59 @@ def StopToStopModelView(request):
     route_num = request.query_params.get('route_num')
     data = ""
     # start_stop = Stop.objects.filter(name="College Street")
-    end_stop = Stop.objects.get(number=1934)
-    print(start_stop, end_stop)
-    return Response(start_stop)
+    # end_stop = Stop.objects.get(number=1934)
+    start_stop = "College Street"
+    end_stop = "Dame Street, stop 1934"
+    return Response(GetAllStops(start_stop, end_stop, "54a", 2))
 
 # def GetStopPrediction(stop_id, route):
 
-def GetAllStops(start_stop, end_stop, route):
+def GetStopModel(stop):
+    """
+    Get the stop model
+    """
+    if stop.find('stop') != -1:
+        stop_split = stop.split(' ')
+        stop_number = stop_split[-1]
+        stop_model = Stop.objects.get(number=stop_number)
+    else:
+        stop_model = Stop.objects.get(name=stop)
+    return stop_model
+
+def GetAllStops(start_stop, end_stop, route, num_stops):
     """
     Get the stops between the start and end stops
     """
-    stops_lists = []
-    return stops_lists
+
+    # get the stop models for both of the stops
+    start_stop_model = GetStopModel(start_stop)
+    end_stop_model = GetStopModel(end_stop)
+
+    # get the route model for the route
+    route_model = Route.objects.get(routeid=route.upper())
+
+    # try outbound first
+    relevant_stops = RouteStop.objects.filter(routeid=route_model, outbound_yn=True)
+    relevant_start_stop = relevant_stops.get(stopnumber=start_stop_model)
+    relevant_end_stop = relevant_stops.get(stopnumber=end_stop_model)
+    if relevant_start_stop.order + num_stops == relevant_end_stop.order:
+        relevant_stops = RouteStop.objects.filter(routeid=route_model, outbound_yn=True, order__range=(relevant_start_stop.order, relevant_end_stop.order))
+        print("outbound")
+    else:
+        # if outbound fails above use inbound
+        relevant_stops = RouteStop.objects.filter(routeid=route_model, outbound_yn=False)
+        relevant_start_stop = relevant_stops.get(stopnumber=start_stop_model)
+        relevant_end_stop = relevant_stops.get(stopnumber=end_stop_model)
+        relevant_stops = RouteStop.objects.filter(routeid=route_model, outbound_yn=False, order__range=(relevant_end_stop.order, relevant_start_stop.order))
+        print("inbound")
+    
+    # get the stops as a list of dictionaries
+    relevant_stops = relevant_stops.values("stopnumber_id")
+
+    # convert list of dictionaries to list of ids
+    relevant_stops = [d['stopnumber_id'] for d in relevant_stops]
+
+    return relevant_stops
 
 # def GetNearestStopByRoute(start_stop, end_stop, route):
 #     relevant_stops = RouteStop.objects.filter(routeid=route)
